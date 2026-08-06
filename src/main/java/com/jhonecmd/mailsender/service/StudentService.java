235d28681fa1cb71c97cce5bee93ef6a97a54708
Jhone-cmd <jhone.1941@gmail.com>
2026-08-06 16:10:40 -0300

    add student component to the service

--- src/main/java/com/jhonecmd/mailsender/service/StudentService.java
+++ src/main/java/com/jhonecmd/mailsender/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.jhonecmd.mailsender.service;
 
 import com.jhonecmd.mailsender.exception.StudentAlreadyExists;
+import com.jhonecmd.mailsender.mail.StudentComponent;
 import com.jhonecmd.mailsender.model.StudentEntity;
 import com.jhonecmd.mailsender.repository.StudentRepository;
 import lombok.AllArgsConstructor;
@@ -11,11 +12,14 @@ import org.springframework.stereotype.Service;
 public class StudentService {
 
     private final StudentRepository studentRepository;
+    private final StudentComponent studentComponent;
 
     public void execute(StudentEntity studentEntity) {
         studentRepository.findByEmail(studentEntity.getEmail()).ifPresent((student) -> {
             throw new StudentAlreadyExists();
         });
+
+        studentComponent.sendSimpleWelcomeEmail(studentEntity);
         studentRepository.save(studentEntity);
         return;
     }
